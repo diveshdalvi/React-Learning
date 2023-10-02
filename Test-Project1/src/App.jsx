17c5e632e6605e671a1a21a760d7554565e95f3f
Divesh Dalvi <dalvidivesh1@gmail.com>
2023-10-02 21:45:03 +0530

    done with project

--- Test-Project1/src/App.jsx
+++ Test-Project1/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './style.css'
 function App() {
 
+  let [counterValue , setCounterValue ] = useState(10)
+  const addValue  = () => {
+    setCounterValue(counterValue+1)
+  }
+  const minusValue = () => {
+    setCounterValue(counterValue - 1)
+  }
   return (
-    <h1 className=''>Project With Vite & React
-    </h1>
+    <div className='flex h-screen items-center justify-center bg-slate-900'>
+    <div className='bg-slate-400 border-2 border-slate-700 inline-flex flex-col  text-center ml-10 mt-10 p-10 text-xl rounded-lg text-white' >
+    <h1>Counter Value : {counterValue}</h1>
+    <button className='bg-slate-900 text-white border-2 border-slate-200 m-5 rounded-lg p-2 active:bg-slate-800'  onClick={addValue}>Increase Value</button>
+    <button className='bg-slate-900 text-white border-2 border-slate-200 m-5 rounded-lg p-2 active:bg-slate-800' onClick={minusValue}>Decrease Value</button>
+    </div>
+   
+    </div>
   )
 }
 
