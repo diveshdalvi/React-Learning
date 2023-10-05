bbc8bb7431e3604d3b55927132145f7281205c00
Divesh Dalvi <dalvidivesh1@gmail.com>
2023-10-05 19:33:56 +0530

    center

--- Test-Project1/src/App.jsx
+++ Test-Project1/src/App.jsx
@@ -10,14 +10,17 @@ function App() {
     setCounterValue(counterValue - 1)
   }
   return (
-    <div className='flex h-screen items-center justify-center bg-slate-900'>
-    <div className='bg-slate-400 border-2 border-slate-700 inline-flex flex-col  text-center ml-10 mt-10 p-10 text-xl rounded-lg text-white' >
+    <>
+    <div className=' bg-slate-950 h-screen flex justify-center align-middle items-center'>
+    <div className='bg-slate-400 border-2 border-slate-700 flex flex-col  text-center ml-10 mt-10 p-10 text-xl rounded-lg text-white h-fit' >
     <h1>Counter Value : {counterValue}</h1>
     <button className='bg-slate-900 text-white border-2 border-slate-200 m-5 rounded-lg p-2 active:bg-slate-800'  onClick={addValue}>Increase Value</button>
     <button className='bg-slate-900 text-white border-2 border-slate-200 m-5 rounded-lg p-2 active:bg-slate-800' onClick={minusValue}>Decrease Value</button>
     </div>
-   
+
     </div>
+    
+    </>
   )
 }
 
